441db5f666b369a7d030639a5707bc0c2cf63f1c
jsmolina1122 <jsmolina1122@gmail.com>
2020-09-10 15:30:13 -0500

    Add exampleone folder

---
+++ exampleone/js/variables.js
@@ -0,0 +1,13 @@
+//Scope
+let nombre = "Fernando";
+//let edad = 35;
+estatura = 1.89;
+const FechaEspecial = "20-10-2020";
+const Estado_Casado = true;
+//const
+//var 
+
+//'Tu Nombre Es '+ nombre + ' y Tu Edad Es ' + edad;
+
+//literal string
+document.write (`Tu Nombre Es ${nombre} y Tu Edad Es ${edad}`);
