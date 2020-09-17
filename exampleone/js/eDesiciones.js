f3acaae02480deb0744725f78442c885120858bb
jsmolina1122 <jsmolina1122@gmail.com>
2020-09-16 20:38:22 -0500

    update if,case,tipo flecha

--- exampleone/js/eDesiciones.js
+++ exampleone/js/eDesiciones.js
@@ -1,5 +1,7 @@
+
 function mostrarMensaje() {
-    let edad=parseInt(document.getById("edad").value);
+    
+    let edad=ParseInt(document.getElementById("edad").value);
 
     // if(edad>=18){
     //     mensaje="sos mayor de edad";
@@ -8,17 +10,17 @@ function mostrarMensaje() {
     //     }
 
       //if con operador terniario    
-let mensaje=(edad>=18)? "sos mayor de edad":"sos menor de edad";
+    let mensaje = (edad >= 18)? "sos mayor de edad":"sos menor de edad";
 
-//alert(mensaje);
-document.write(mensaje);
+    alert(mensaje);
+    //document.write(mensaje);
 }
     
 
 function mostrarMensajedos(){
-     let edadp=parseInt(document.getById("edadp").value);
-     let estaturap=parseInt(document.getById("estaturap").value);
-     let pesop=parseInt(document.getById("pesop").value);
+     let edadp=parseInt(document.getElementById("edadp").value);
+     let estaturap=parseInt(document.getElementById("estaturap").value);
+     let pesop=parseInt(document.getElementById("pesop").value);
 
         let parametroedad=80;
         let parametroestatura=1.40;
@@ -47,4 +49,65 @@ function mostrarMensajedos(){
         mensajeDesicion="No apto para realizar el deporte";
     }
     alert(mensajeDesicion);
+}
+
+function mostrarmensajetres(){
+    let productname=document.getElementById("productname").value;
+    let price=0;
+     const product_one='a';
+     const product_two='b';
+     const product_three='c';
+     const MESSAGE='product invalid!!'
+
+    // if (productname.toUppercase()==product_one) {
+    //     price=2000;
+    //     //alert(price);
+    // } else {
+    //     if(productname.toUppercase()==product_two){
+    //         price=2200;
+    //        // alert(price);
+    //     }
+    //     else{
+    //         if (productname.toUppercase()==product_three) {
+    //            price=1800; 
+    //            //alert(price);
+    //         }else{
+    //             alert(MESSAGE.toUppercase());
+    //         }
+    //     }
+        
+    // }
+    // alert(price);
+    switch (productname.toUperCase()) {
+        case product_one:
+            price=20000;
+            break;
+        case product_two:
+                price=22000;
+                break;
+        case product_three:
+                price=18000;
+                break;
+    
+        default:
+            alert(MESSAGE.toUpperCase());
+            break;
+    }
+    alert(Intl.NumberFormat.format(price));
+}
+
+//function prenderapagar(interruptor) {
+    let prenderapagar=interruptor=>{
+
+    let pic;
+
+    // if (interruptor==0) {
+    //     alert('OFF')
+        
+    // } else {
+    //     alert('ON')
+        
+    // }
+    let mensaje=(interruptor==0)?"OFF":"ON"
+    alert(mensaje);
 }
